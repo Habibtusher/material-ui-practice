127c9cfc47dfb96ffc14c1fb7fde4ad336239157
Habibtusher <tusherhrt@gmail.com>
2022-04-07 11:43:44 +0600

    07

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,12 +1,20 @@
-
-import React from "react";
+import React, { useState } from "react";
 import Container from "@material-ui/core/Container";
 import AcUnitOutlinedIcon from "@material-ui/icons/AcUnitOutlined";
 import SendOutlinedIcon from "@material-ui/icons/SendOutlined";
 import KeyboardArrowRightOutlinedIcon from "@material-ui/icons/KeyboardArrowRightOutlined";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core";
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  makeStyles,
+} from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles({
   btn: {
@@ -16,33 +24,103 @@ const useStyles = makeStyles({
       backgroundColor: "blue",
     },
   },
-  title:{
-    textDecoration: 'underline',
-    marginBottom: 25
-  }
+  title: {
+    textDecoration: "underline",
+    marginBottom: 25,
+  },
+  field: {
+    marginTop: "20px",
+    marginBottom: "20px",
+    display: "block",
+  },
 });
 
 export default function Create() {
   const classes = useStyles();
+  const history = useHistory();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [titleError, setTitleError] = useState("");
+  const [detailsError, setDetailsError] = useState("");
+  const [category, setCategory] = useState("todos");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTitleError(false);
+    setDetailsError(false);
+    if (title == "") {
+      setTitleError(true);
+    }
+    if (detailsError == "") {
+      setDetailsError(true);
+    }
+
+    if (title && details) {
+      fetch("http://localhost:8000/notes", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ title, details, category }),
+      }).then(()=> history.push('/'))
+    }
+  };
 
   return (
     <Container>
-      <Typography 
-      className={classes.title}
-      variant="h3" 
-      color="textSecondary" 
-      gutterBottom>
+      <Typography
+        className={classes.title}
+        variant="h3"
+        color="textSecondary"
+        gutterBottom
+      >
         Create New a Notes
       </Typography>
-      <Button
-        
-        onClick={() => console.log("you clicked me")}
-        variant="contained"
-        color="primary"
-        endIcon={<KeyboardArrowRightOutlinedIcon />}
-      >
-        Submit
-      </Button>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          id="outlined-basic"
+          label="Note Title"
+          variant="outlined"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.field}
+          id="outlined-basic"
+          label="Details"
+          variant="outlined"
+          fullWidth
+          multiline
+          rows={4}
+          required
+          error={detailsError}
+        />
+        <FormControl className={classes.field}>
+          <FormLabel>Note Category</FormLabel>
+          <RadioGroup
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <FormControlLabel value="money" control={<Radio />} label="Money" />
+            <FormControlLabel value="todos" control={<Radio />} label="ToDos" />
+            <FormControlLabel
+              value="remainde"
+              control={<Radio />}
+              label="Remainder"
+            />
+            <FormControlLabel value="work" control={<Radio />} label="Work" />
+          </RadioGroup>
+        </FormControl>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          endIcon={<KeyboardArrowRightOutlinedIcon />}
+        >
+          Submit
+        </Button>
+      </form>
     </Container>
   );
 }
